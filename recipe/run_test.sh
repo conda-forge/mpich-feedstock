8406e9acd57bb2d1e1c7fe425bad67a91eef2762
Min RK <benjaminrk@gmail.com>
2019-01-24 12:05:20 +0100

    add mpich-mpicc,cxx,fort outputs
    these depend on conda compilers as an easy way to install the mpi compiler and its requirements
    
    mpich package is unchanged, and actually ships with the mpi compiler wrappers. They just may not work without the compiler packages
    
    (they do work fine on mac, but not linux)

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 command -v mpichversion
 mpichversion
@@ -14,23 +15,38 @@ mpif90 -show
 
 command -v mpiexec
 
-pushd tests
+# test files doesn't work with multiple outputs
+pushd "$RECIPE_DIR/tests"
 
 function mpi_exec() {
   # use pipes to avoid O_NONBLOCK issues on stdin, stdout
   mpiexec -launcher fork $@ 2>&1 </dev/null | cat
 }
 
-mpicc $CFLAGS $LDFLAGS helloworld.c -o helloworld_c
-mpi_exec -n 4 ./helloworld_c
+if [[ $PKG_NAME == "mpich" ]]; then
+  cat >test.py <<EOF
+import os
+print(os.getpid())
+EOF
+  mpi_exec -n 4 python test.py
+fi
 
-mpicxx $CXXFLAGS $LDFLAGS helloworld.cxx -o helloworld_cxx
-mpi_exec -n 4 ./helloworld_cxx
+if [[ $PKG_NAME == "mpich-mpicc" ]]; then
+  mpicc $CFLAGS $LDFLAGS helloworld.c -o helloworld_c
+  mpi_exec -n 4 ./helloworld_c
+fi
 
-mpif77 $FFLAGS $LDFLAGS helloworld.f -o helloworld_f
-mpi_exec -n 4 ./helloworld_f
+if [[ $PKG_NAME == "mpich-mpicxx" ]]; then
+  mpicxx $CXXFLAGS $LDFLAGS helloworld.cxx -o helloworld_cxx
+  mpi_exec -n 4 ./helloworld_cxx
+fi
 
-mpif90 $FFLAGS $LDFLAGS helloworld.f90 -o helloworld_f90
-mpi_exec -n 4 ./helloworld_f90
+if [[ $PKG_NAME == "mpich-mpifort" ]]; then
+  mpif77 $FFLAGS $LDFLAGS helloworld.f -o helloworld_f
+  mpi_exec -n 4 ./helloworld_f
+
+  mpif90 $FFLAGS $LDFLAGS helloworld.f90 -o helloworld_f90
+  mpi_exec -n 4 ./helloworld_f90
+fi
 
 popd
